33ea23ac6ab51f104c0ff9701c7e54911b0f8297
santiago <samiri@uni-bremen.de>
2024-05-04 19:00:25 +0200

    clean up;

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,5 +1,5 @@
-from qiskit import QuantumCircuit, transpile, assemble, Aer, execute
-from qiskit.visualization import plot_bloch_multivector, plot_histogram
+"""First Qiskit program."""
+from qiskit import QuantumCircuit
 
 # Create a Quantum Circuit acting on a quantum register of one qubit
 circ = QuantumCircuit(1)
@@ -7,22 +7,6 @@ circ = QuantumCircuit(1)
 # Add a H gate on qubit 0, putting this qubit in superposition.
 circ.h(0)
 
-# Add a measure gate to see the state.
-circ.measure_all()
-
-# Use Aer's qasm_simulator
-simulator = Aer.get_backend('qasm_simulator')
-
-# Execute the circuit on the qasm simulator
-job = execute(circ, simulator, shots=1000)
-
-# Grab the results from the job.
-result = job.result()
-
-# Returns counts
-counts = result.get_counts(circ)
-print("\nTotal count for 0 and 1 are:",counts)
-
 # Draw the circuit
-print("\nHere is the circuit:")
-circ.draw()
+print("\nHello world, Here is the circuit:")
+print(circ)
