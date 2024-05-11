76527b03722668d7452cdd98a12af84562476f89
santiago <samiri@uni-bremen.de>
2024-05-11 19:00:25 +0200

    NOT example

---
+++ cirq_hidary/cirq-basic.py
@@ -0,0 +1,28 @@
+"""simple program in Cirq that creates a quantum circuit with
+one qubit and applies NOT gate to it."""
+
+import cirq
+
+# Pick a qubit.
+qubit = cirq.GridQubit(0, 0)
+
+# Create a circuit
+circuit = cirq.Circuit(
+    cirq.X(qubit),  # NOT gate
+    cirq.measure(qubit, key='m')  # measurement
+)
+
+print("\nHello world, Here is the circuit:")
+print(circuit)
+
+# Simulate the circuit
+simulator = cirq.Simulator()
+
+# Simulate the circuit several times
+result = simulator.run(circuit, repetitions=20)
+
+# Print the results
+print("\nMeasurement results:")
+print(result)
+print("\nMeasurement results histogram:")
+print(result.histogram(key='m'))
