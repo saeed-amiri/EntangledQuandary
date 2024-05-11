8560d496df43af6e696560b7d1f2c2f993bd78a8
santiago <samiri@uni-bremen.de>
2024-05-11 19:08:13 +0200

    add hadamard gate

--- cirq_hidary/cirq-basic.py
+++ cirq_hidary/cirq-basic.py
@@ -26,3 +26,18 @@ print("\nMeasurement results:")
 print(result)
 print("\nMeasurement results histogram:")
 print(result.histogram(key='m'))
+
+# Apply Hadamard gate
+circuit.append(cirq.H(qubit))
+
+print("\nHello world, Here is the circuit after applying Hadamard gate:")
+print(circuit)
+
+# Simulate the circuit
+result = simulator.run(circuit, repetitions=20)
+
+# Print the results
+print("\nMeasurement results:")
+print(result)
+print("\nMeasurement results histogram:")
+print(result.histogram(key='m'))
