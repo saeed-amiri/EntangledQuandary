9f0d52342a5259d901a009ec0d8da112cebc766e
santiago <samiri@uni-bremen.de>
2024-05-18 16:46:06 +0200

    update the doc

--- cirq_hidary/bell_states.py
+++ cirq_hidary/bell_states.py
@@ -1,12 +1,42 @@
 """
 Script for preparing the Bell states using Cirq.
-|Φ+⟩ = (|00⟩ + |11⟩)/√2
-0: ───H───@───
-          │
-1: ───────X───
+In Bell's original paper, he described four maximally entangled states.
+These states are known as the Bell states.
+The Bell states are prepared using a Hadamard gate and a CNOT gate.
+The four Bell states are:
 
+|Φ+⟩ = (|00⟩ + |11⟩)/√2
 |Φ-⟩ = (|00⟩ - |11⟩)/√2
 |Ψ+⟩ = (|01⟩ + |10⟩)/√2
+|Ψ-⟩ = (|01⟩ - |10⟩)/√2
+
+In Bell statesm for |Φ+⟩, the Hadamard gate is applied to the first qubit
+and the CNOT gate is applied to the first and second qubits.
+The circuit for |Φ+⟩ is:
+H|00> = (H ⊗ I) (|0⟩ ⊗ |0⟩)
+      -> (H|0⟩) ⊗ |0⟩ -> (|0⟩ + |1⟩) ⊗ |0⟩ -> |00⟩ + |11⟩
+CNOT|00> = (CNOT ⊗ I) (|0⟩ ⊗ |0⟩)
+         -> (CNOT|0⟩) ⊗ |0⟩ -> |0⟩ ⊗ |0⟩ -> |00⟩
+         -> (CNOT|1⟩) ⊗ |0⟩ -> |1⟩ ⊗ |1⟩ -> |11⟩
+-> |Φ+⟩ = (|00⟩ + |11⟩)/√2
+
+The circuit for |Ψ+⟩ is:
+H|01> = (H ⊗ I) (|0⟩ ⊗ |1⟩)
+      -> (H|0⟩) ⊗ |1⟩ -> (|0⟩ + |1⟩) ⊗ |1⟩ -> |01⟩ + |11⟩
+CNOT|01> = (CNOT ⊗ I) (|0⟩ ⊗ |1⟩)
+         -> (CNOT|0⟩) ⊗ |1⟩ -> |0⟩ ⊗ |1⟩ -> |01⟩
+         -> (CNOT|1⟩) ⊗ |1⟩ -> |1⟩ ⊗ |0⟩ -> |10⟩
+-> |Ψ+⟩ = (|01⟩ + |10⟩)/√2
+
+For |Φ-⟩ and |Ψ-⟩, the Pauli-Z gate is applied to the second qubit.
+The Pauli-Z gate is a quantum gate that acts as the identity on the |0⟩
+state and as a phase flip (sign change) on the |1⟩ state.
+The Pauli-Z gate is represented by the following matrix:
+Z = |1 0|
+    |0 -1|
+The circuit for |Φ-⟩ calculates by applying the Pauli-Z gate to  |Φ+⟩.
+The circuit for |Ψ-⟩ calculates by applying the Pauli-Z gate to  |Ψ+⟩.
+|Φ-⟩ = (|00⟩ - |11⟩)/√2
 |Ψ-⟩ = (|01⟩ - |10⟩)/√2
 """
 
