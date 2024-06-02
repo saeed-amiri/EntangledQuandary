244f534cf3f83da5819903f736cb0e70977cd7bf
santiago <samiri@uni-bremen.de>
2024-06-02 15:24:37 +0200

    initiate a circuit

--- cirq_hidary/bell_inequality.py
+++ cirq_hidary/bell_inequality.py
@@ -7,7 +7,32 @@ Here is the main idea:
     4. Alice and Bob send the results to the referee
     5. The referee checks the results:
         if a(x) XOR b(y) == xy they win
-        else they lose
+        else they lose(
 In quantum version of the game, Alice and Bob can set a strategy to win
 """
 # pylint: disable=import-error
+
+import numpy as np
+
+import cirq
+
+
+def make_bell_test_circuit() -> None:
+    """make a bell test circuit"""
+    # Qubit for Alice, Bob, Refree
+    alice: "cirq.devices.grid_qubit.GridQubit" = cirq.GridQubit(0, 0)
+    bob: "cirq.devices.grid_qubit.GridQubit" = cirq.GridQubit(1, 0)
+
+    alice_refree: "cirq.devices.grid_qubit.GridQubit" = cirq.GridQubit(0, 1)
+    bob_refree: "cirq.devices.grid_qubit.GridQubit" = cirq.GridQubit(1, 1)
+
+    circuit: "cirq.circuits.circuit.Circuit" = cirq.Circuit()
+
+
+def main() -> None:
+    # Create a circuit
+    circuit = make_bell_test_circuit()
+
+
+if __name__ == '__main__':
+    main()
