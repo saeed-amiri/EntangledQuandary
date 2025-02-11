ce9a71533476ea1cbaa9c69eb69c8812d20feadd
saeed amiri <sdramiri952@gmail.com>
2025-02-11 14:48:08 +0100

    Doc: clean up

--- algorithms/cirq/bell_inequality.py
+++ algorithms/cirq/bell_inequality.py
@@ -1,7 +1,7 @@
 """
 simulate a circuit that mimic the Bell inequality
 Here is the main idea:
-    1. A rferee prepares a pair of entangled qubits
+    1. A referee prepares a pair of entangled qubits
     2. The referee sends one qubit to Alice (x) and the other to Bob (y)
     3. Alice and Bob measure their qubits (a(x), b(y))
     4. Alice and Bob send the results to the referee
